420dffe49af373e02da521114180f3eb64007bdf
A809704 <hajar.jahoui@atos.net>
2021-04-27 11:06:02 +0200

    update action method get

--- application/controllers/PieceJointeController.php
+++ application/controllers/PieceJointeController.php
@@ -62,10 +62,12 @@ class PieceJointeController extends Zend_Controller_Action
 
         // Cas dossier
         $piece_jointe = null;
+        $piece_jointe_signe = null;
         if ($this->_request->type == 'dossier') {
             $type = 'dossier';
             $identifiant = $this->_request->id;
             $piece_jointe = $DBused->affichagePieceJointe('dossierpj', 'piecejointe.ID_PIECEJOINTE', $this->_request->idpj);
+            $piece_jointe_signe = $DBused->affichagePieceJointeSigne('dossierpj', 'piecejointe.ID_PIECEJOINTE', $this->_request->idpj);
         } elseif ($this->_request->type == 'etablissement') { // Cas établissement
             $type = 'etablissement';
             $identifiant = $this->_request->id;
@@ -75,24 +77,34 @@ class PieceJointeController extends Zend_Controller_Action
             $identifiant = $this->_request->id;
             $piece_jointe = $DBused->affichagePieceJointe('datecommissionpj', 'piecejointe.ID_PIECEJOINTE', $this->_request->idpj);
         }
-
+        
         if (
-            !$piece_jointe
-            || empty($piece_jointe)
+            (!$piece_jointe && !$piece_jointe_signe)
+            || (empty($piece_jointe) && empty($piece_jointe_signe))
         ) {
             throw new Zend_Controller_Action_Exception('Cannot find piece jointe for id '.$this->_request->idpj, 404);
         }
 
         $piece_jointe = $piece_jointe[0];
+        $piece_jointe_signe = $piece_jointe_signe[0];
 
         $filepath = $this->store->getFilePath($piece_jointe, $type, $identifiant);
         $filename = $this->store->getFormattedFilename($piece_jointe, $type, $identifiant);
 
+        $filepath_signe = $this->store->getFilePath($piece_jointe_signe, $type, $identifiant);
+        $filename_signe = $this->store->getFormattedFilename($piece_jointe_signe, $type, $identifiant);
+
         $this->_helper->layout()->disableLayout();
         $this->_helper->viewRenderer->setNoRender(true);
 
-        if (!is_readable($filepath)) {
-            throw new Zend_Controller_Action_Exception('Cannot read file '.$filepath, 404);
+        if(empty($piece_jointe)){
+            $filepath = null;
+        } elseif (empty($piece_jointe_signe)){
+            $filepath_signe = null;
+        }
+       
+        if (!is_readable($filepath) && !is_readable($filepath_signe)) {
+            throw new Zend_Controller_Action_Exception('Cannot read file '.$filepath.'<br/>'.$filepath_signe, 404);
         }
 
         ob_get_clean();
@@ -104,6 +116,16 @@ class PieceJointeController extends Zend_Controller_Action
         header('Content-Type: application/octet-stream');
 
         readfile($filepath);
+
+        ob_get_clean();
+
+        header('Pragma: public');
+        header('Expires: -1');
+        header('Cache-Control: public, must-revalidate, post-check=0, pre-check=0');
+        header('Content-Disposition: attachment; filename="'.$filename_signe.'"');
+        header('Content-Type: application/octet-stream');
+
+        readfile($filename_signe);
         exit();
     }
 
