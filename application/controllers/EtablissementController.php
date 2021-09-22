7a64f8d7ebf3c74c03572fe68ecc90af6c4b882e
A709197 <maxime.merrien@atos.net>
2021-09-22 11:34:08 +0200

    ajout lecture dans textarea etablissement effectife et deagagement

--- application/controllers/EtablissementController.php
+++ application/controllers/EtablissementController.php
@@ -473,6 +473,11 @@ class EtablissementController extends Zend_Controller_Action
         $this->_helper->layout->setLayout('etablissement');
         $this->view->headScript()->appendFile('/js/tinymce.min.js','text/javascript');
         $service_etablissement = new Service_Etablissement();
+
+        $modelEffectifDegagement = new Model_DbTable_EffectifEtDegagement();
+        $this->view->EffectifDegagement =$modelEffectifDegagement->getEffectifEtDegagement(1);
+        
+        
         $this->view->etablissement = $service_etablissement->get($this->_request->id);
         $this->view->avis = $service_etablissement->getAvisEtablissement($this->view->etablissement['general']['ID_ETABLISSEMENT'], $this->view->etablissement['general']['ID_DOSSIER_DONNANT_AVIS']);
         $this->view->historique = $service_etablissement->getHistorique($this->_request->id);
--- application/models/DbTable/EffectifEtDegagement.php
+++ application/models/DbTable/EffectifEtDegagement.php
@@ -2,7 +2,7 @@
 
 class Model_DbTable_EffectifEtDegagement extends Zend_Db_Table_Abstract
 {
-    protected $_name = 'EffectifEtDegagement'; // Nom de la base
+    protected $_name = 'EffectifDegagement'; // Nom de la base
     protected $_primary = 'ID_EFFECTIF_DEGAGEMENT'; // Clé primaire
 
     /**
@@ -13,8 +13,24 @@ class Model_DbTable_EffectifEtDegagement extends Zend_Db_Table_Abstract
     public function getEffectifEtDegagement($idInput)
     {
         $select = 'SELECT *
-            FROM EffectifEtDegagement 
-            WHERE EffectifEtDegagement.ID_EFFECTIF_DEGAGEMENT =' .$idInput.';';
+            FROM EffectifDegagement 
+            WHERE EffectifDegagement.ID_EFFECTIF_DEGAGEMENT =' .$idInput.';';
+
+        return $this->getAdapter()->fetchAll($select);
+    }
+
+    public function getEffectif($idInput){
+        $select = 'SELECT EFFECTIF
+            FROM EffectifDegagement 
+            WHERE EffectifDegagement.ID_EFFECTIF_DEGAGEMENT =' .$idInput.';';
+
+        return $this->getAdapter()->fetchAll($select);
+    }
+
+    public function getDegagement($idInput){
+        $select = 'SELECT DEGAGEMENT
+            FROM EffectifDegagement 
+            WHERE EffectifDegagement.ID_EFFECTIF_DEGAGEMENT =' .$idInput.';';
 
         return $this->getAdapter()->fetchAll($select);
     }
