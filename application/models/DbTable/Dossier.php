ac9d371f798577e77056193c7155d3a6e192e803
Maxime Merrien <maxime.merrien@atos.net>
2021-11-24 16:47:41 +0100

    Ajout type et nature dossier sur tableau de bord platau

--- application/models/DbTable/Dossier.php
+++ application/models/DbTable/Dossier.php
@@ -488,10 +488,25 @@ class Model_DbTable_Dossier extends Zend_Db_Table_Abstract
     /**
      * Retourne la liste des dossiers provenant de plat'au et n etant lie a aucun etablissement 
      */
-    public function getAllDossierPlatAU(){
-        $select = "
-        SELECT dossier.* FROM dossier WHERE dossier.ID_DOSSIER NOT IN  (SELECT etablissementdossier.ID_DOSSIER from etablissementdossier) AND dossier.ID_PLATAU IS NOT NULL;
-        ";
+    public function getAllDossierPlatAU()
+    {
+        $select = $this->select()->setIntegrityCheck(false)
+            ->from(array('d' => 'dossier'))
+            ->columns(
+                array(
+                    'NB_URBA' => new Zend_Db_Expr("( SELECT group_concat(dossierdocurba.NUM_DOCURBA, ', ')
+                        FROM dossier
+                        INNER JOIN dossierdocurba ON dossierdocurba.ID_DOSSIER = dossier.ID_DOSSIER
+                        WHERE dossier.ID_DOSSIER = d.ID_DOSSIER
+                        LIMIT 1)")
+                )
+            )
+            ->join(array('dt' => 'dossiertype'), 'd.TYPE_DOSSIER = dt.ID_DOSSIERTYPE', 'dt.LIBELLE_DOSSIERTYPE')
+            ->join(array('dn' => 'dossiernature'), 'd.ID_DOSSIER = dn.ID_DOSSIER')
+            ->join(array('dnl' => 'dossiernatureliste'), 'dn.ID_NATURE = dnl.ID_DOSSIERNATURE', 'dnl.LIBELLE_DOSSIERNATURE')
+            ->where('d.ID_DOSSIER NOT IN (SELECT etablissementdossier.ID_DOSSIER from etablissementdossier)')
+            ->where('d.ID_PLATAU IS NOT NULL')
+        ;
         return $this->getAdapter()->fetchAll($select);
     }
 }
