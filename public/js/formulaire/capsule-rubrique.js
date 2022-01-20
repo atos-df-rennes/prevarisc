5e6064c9fcb6ae5de08f3d530ad8522d4b56c568
Maxime Merrien <maxime.merrien@atos.net>
2022-01-20 11:57:03 +0100

    Suppression de toute la table si une seule ligne

--- public/js/formulaire/capsule-rubrique.js
+++ public/js/formulaire/capsule-rubrique.js
@@ -21,13 +21,33 @@ $(document).ready(function() {
 
     $('.delete-rubrique').on('click', function() {
         const id = this.getAttribute('data-id')
+
         const parentDiv = $(this).parent().parent()
+        const parentTable = $(this).closest('table')
+        const nbOfRows = parentTable.children('tbody').children('tr').length
+
+        const parentTableDiv = $(this).closest('.row-fluid')
+        const parentDivTitlesDiv = parentTableDiv.prev().children()
+        let parentDivTitleDiv = null
+
+        for (let i = 0; i < parentDivTitlesDiv.length; i++) {
+            if (parentDivTitlesDiv[i].className === 'span7 offset1') {
+                parentDivTitleDiv = parentDivTitlesDiv[i]
+            }
+        }
 
         $.ajax({
             url: '/formulaire/delete-rubrique/rubrique/'+id,
             type: 'POST',
             success: function() {
-                parentDiv.remove()
+                if (nbOfRows === 1) {
+                    parentTable.remove()
+                    if (parentDivTitleDiv !== null) {
+                        parentDivTitleDiv.remove()
+                    }
+                } else {
+                    parentDiv.remove()
+                }
             },
             error: function() {
                 return false
