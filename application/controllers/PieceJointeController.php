dd93ca565e87ec3f7136e36a86b5d908f418ba6c
Maxime MERRIEN <48253852+A709197@users.noreply.github.com>
2022-02-01 10:58:44 +0100

    Correction de l'ouverture des PJs Prevarisc sur un dossier Plat'AU (#26)
    * Correction ouverture PJ Prevarisc sur dossier Plat'AU
    
    * Suppression physique PJs Prevarisc sur dossier Plat'AU
    
    * Correction ajout PJ Prevarisc sur dossier Plat'AU

--- application/controllers/PieceJointeController.php
+++ application/controllers/PieceJointeController.php
@@ -153,7 +153,20 @@ class PieceJointeController extends Zend_Controller_Action
             // Sauvegarde de la BDD
             $nouvellePJ->save();
 
-            $file_path = $this->store->getFilePath($nouvellePJ, $this->_getParam('type'), $this->_getParam('id'), true);
+            // FIXME Solution temporaire pour ouvrir les PJs provenant de Plat'AU
+            $modelDossier = new Model_DbTable_Dossier();
+            $dossier = $modelDossier->find($this->_getParam('id'))->current();
+
+            if ($dossier['ID_PLATAU'] !== null) {
+                $file_path = implode(DS, array(
+                    REAL_DATA_PATH,
+                    'uploads',
+                    'pieces-jointes',
+                    $nouvellePJ->ID_PIECEJOINTE.$nouvellePJ->EXTENSION_PIECEJOINTE
+                ));
+            } else {
+                $file_path = $this->store->getFilePath($nouvellePJ, $this->_getParam('type'), $this->_getParam('id'), true);
+            }
 
             // On check si l'upload est okay
             $linkPj = null;
@@ -270,7 +283,22 @@ class PieceJointeController extends Zend_Controller_Action
                 $pj != null
                 && $DBitem != null
             ) {
-                $file_path = $this->store->getFilePath($pj, $this->_request->type, $this->_request->id);
+                // FIXME Solution temporaire pour ouvrir les PJs provenant de Plat'AU
+                $modelDossier = new Model_DbTable_Dossier();
+
+                $dossier = $modelDossier->find($this->_request->id)->current();
+
+                if ($dossier['ID_PLATAU'] !== null) {
+                    $file_path = implode(DS, array(
+                        REAL_DATA_PATH,
+                        'uploads',
+                        'pieces-jointes',
+                        $pj->ID_PIECEJOINTE.$pj->EXTENSION_PIECEJOINTE
+                    ));
+                } else {
+                    $file_path = $this->store->getFilePath($pj, $this->_request->type, $this->_request->id);
+                }
+
                 $miniature_pj = $pj;
                 $miniature_pj['EXTENSION_PIECEJOINTE'] = '.jpg';
                 $miniature_path = $this->store->getFilePath($miniature_pj, 'etablissement_miniature', $this->_request->id);
@@ -307,6 +335,7 @@ class PieceJointeController extends Zend_Controller_Action
         // Modèle
         $DBused = new Model_DbTable_PieceJointe();
 
+        // FIXME Pourquoi on a une liste alors qu'on a tout le temps maximum 1 résultat ????
         // Cas dossier
         if ($this->_request->type == 'dossier') {
             $listePj = $DBused->affichagePieceJointe('dossierpj', 'dossierpj.ID_PIECEJOINTE', $this->_request->idpj);
@@ -324,7 +353,22 @@ class PieceJointeController extends Zend_Controller_Action
             return;
         }
 
-        $file_path = $this->store->getFilePath($pj, $this->_request->type, $this->_request->id);
+        // FIXME Solution temporaire pour ouvrir les PJs provenant de Plat'AU
+        $modelDossier = new Model_DbTable_Dossier();
+
+        $dossier = $modelDossier->find($this->_request->id)->current();
+
+        if ($dossier['ID_PLATAU'] !== null) {
+            $file_path = implode(DS, array(
+                REAL_DATA_PATH,
+                'uploads',
+                'pieces-jointes',
+                $pj['ID_PIECEJOINTE'].$pj['EXTENSION_PIECEJOINTE']
+            ));
+        } else {
+            $file_path = $this->store->getFilePath($pj, $this->_request->type, $this->_request->id);
+        }
+
         $this->view->exists = file_exists($file_path);
 
         if ($this->view->exists) {
