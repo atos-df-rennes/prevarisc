43ff1bae65420830ac6981700ac577fe4238b06d
Maxime Merrien <maxime.merrien@atos.net>
2022-02-04 18:01:35 +0100

    Début construction tableau infos couches

--- public/js/main.js
+++ public/js/main.js
@@ -169,18 +169,12 @@ function initViewer(divId, ignKey, center, description, autoconfPath) {
                 y : center[1],
                 projection : "EPSG:4326"
             },
-            layersOptions : {
-                // COUCHE DE BASE            
-                "GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD": {},
-                "ORTHOIMAGERY.ORTHOPHOTOS" : {}
-            },
             // Outils additionnels à proposer sur la carte
             controlsOptions : {
                 "layerSwitcher" : {},
                 "search" : {},
                 "orientation" : {},
                 "graphicscale" : {},
-                //"mouseposition" : {},
                 "graticule" : {},
                 "length" : {},
                 "area" : {},
@@ -196,36 +190,158 @@ function initViewer(divId, ignKey, center, description, autoconfPath) {
 }
 
 // FIXME Ajouter les couches au format WFS & WMTS
-function addUserLayers(viewer, ignKey, wmsLayers) {
-    // Ajout des couches wms
-    if(wmsLayers && wmsLayers.length > 0) {
-        for(var i = 0; i < wmsLayers.length; i++) {
-            var source = new ol.source.TileWMS({
+function addUserLayers(viewer, ignKey, layers) {
+    const wmsLayers = layers.filter(layer => (layer.TYPE_COUCHECARTO === 'WMS'))
+    addWmsLayers(viewer, ignKey, wmsLayers)
+
+    const wfsLayers = layers.filter(layer => (layer.TYPE_COUCHECARTO === 'WFS'))
+    // TODO addWfsLayers
+
+    const wmtsLayers = layers.filter(layer => (layer.TYPE_COUCHECARTO === 'WMTS'))
+    // TODO addWmtsLayers
+    addWmtsLayers(viewer, ignKey, wmtsLayers)
+
+    return viewer;
+}
+
+function addWmsLayers(viewer, ignKey, wmsLayers) {
+    // Ajout des couches WMS
+    if (wmsLayers && wmsLayers.length > 0) {
+        for (let i = 0; i < wmsLayers.length; i++) {
+            const source = new ol.source.TileWMS({
                 url: wmsLayers[i].URL_COUCHECARTO.replace('\{key\}', ignKey),
                 params: {
                     'LAYERS': wmsLayers[i].LAYERS_COUCHECARTO,
                     'FORMAT': wmsLayers[i].FORMAT_COUCHECARTO,
                     'TILED': true
                 }
-            });
-            var layer = new ol.layer.Tile({
+            })
+
+            const layer = new ol.layer.Tile({
                 source: source,
                 visible: wmsLayers[i].TRANSPARENT_COUCHECARTO == 0 ? false : true
-            });
+            })
+
             viewer.getLibMap().addLayer(layer);
 
             // On renomme les couches utilisateurs
             $('.GPlayerName').eq(-(viewer.getLibMap().getLayers().getLength())).text(wmsLayers[i].NOM_COUCHECARTO)
-            .attr('title', wmsLayers[i].NOM_COUCHECARTO);
+            .attr('title', wmsLayers[i].NOM_COUCHECARTO)
         }
     }
+}
 
-    return viewer;
+function addWmtsLayers(viewer, ignKey, wmtsLayers) {
+    getCapabilities(ignKey, 'wmts')
+
+    var resolutions = [
+            156543.03392804103,
+            78271.5169640205,
+            39135.75848201024,
+            19567.879241005125,
+            9783.939620502562,
+            4891.969810251281,
+            2445.9849051256406,
+            1222.9924525628203,
+            611.4962262814101,
+            305.74811314070485,
+            152.87405657035254,
+            76.43702828517625,
+            38.218514142588134,
+            19.109257071294063,
+            9.554628535647034,
+            4.777314267823517,
+            2.3886571339117584,
+            1.1943285669558792,
+            0.5971642834779396,
+            0.29858214173896974,
+            0.14929107086948493,
+            0.07464553543474241
+    ] ;
+
+    // // Ajout des couches WMTS
+    if (wmtsLayers && wmtsLayers.length > 0) {
+        for (let i = 0; i < wmtsLayers.length; i++) {
+            // FIXME Remplacer les valeurs en dur par le retour du GetCapabilities ?
+            const source = new ol.source.WMTS({
+                url: wmtsLayers[i].URL_COUCHECARTO.replace('\{key\}', ignKey),
+                layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2',
+                matrixSet: 'PM',
+                format: wmtsLayers[i].FORMAT_COUCHECARTO,
+                tileGrid: new ol.tilegrid.WMTS({
+                    origin: [-20037508,20037508],
+                    resolutions: resolutions,
+                    matrixIds:["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"],
+                }),
+                style: 'normal'
+            })
+
+            const layer = new ol.layer.Tile({
+                source: source,
+                visible: wmtsLayers[i].TRANSPARENT_COUCHECARTO == 0 ? false : true
+            })
+
+            viewer.getLibMap().addLayer(layer);
+
+            // On renomme les couches utilisateurs
+            $('.GPlayerName').eq(-(viewer.getLibMap().getLayers().getLength())).text(wmtsLayers[i].NOM_COUCHECARTO)
+            .attr('title', wmtsLayers[i].NOM_COUCHECARTO)
+        }
+    }
+}
+
+function getCapabilities(ignKey, format) {
+    $.get('https://wxs.ign.fr/' + ignKey + '/geoportail/' + format + '?SERVICE=' + format + '&REQUEST=GetCapabilities', {}, function (result) {
+        var result = $(result)
+        var layers = []
+
+        result.find('Layer').each(function (index, layer) {
+            var layer = $(layer)
+
+            var layerFormat = null
+            var layerName = null
+            var layerStyle = null
+
+            layer.children().each(function (index, child) {
+                if (child.localName === 'Format') {
+                    layerFormat = child.innerHTML
+                    
+                    if (layerFormat !== 'image/png' && layerFormat !== 'image/jpeg') {
+                        layerName = null
+                        layerStyle = null
+                        return
+                    }
+                }
+
+                if (child.localName === 'Identifier') {
+                    layerName = child.innerHTML
+                }
+
+                if (child.localName === 'Style') {
+                    $(child).children().each(function (index, ch) {
+                        if (ch.localName === 'Identifier') {
+                            layerStyle = ch.innerHTML
+                        }
+                    })
+                }
+            })
+
+            // TODO Ajouter les options dans le tableau
+            if (layerName !== null) {
+                layers[layerName] = {
+                    format: layerFormat,
+                    style: layerStyle
+                }
+            }
+        })
+
+        console.log(layers)
+    })
 }
 
 function putMarkerAt(viewer, center, nbCouches) {
     // Si on a déjà un marker, on le retire
-    if(viewer.getLayers().getLength() != nbCouches) {
+    if (viewer.getLayers().getLength() != nbCouches) {
         var toRemove = viewer.getLayers().item(viewer.getLayers().getLength()-1);
         viewer.removeLayer(toRemove);
     }
