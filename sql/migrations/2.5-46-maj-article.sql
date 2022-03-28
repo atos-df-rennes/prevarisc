f8d65f5220c010e0f402e6e439369d5e30b23b96
valentinrouxelrennes <valentin.rouxel@atos.net>
2022-03-28 14:48:24 +0200

    ajout migration update prescription

--- sql/migrations/2.5-46-maj-article.sql
+++ sql/migrations/2.5-46-maj-article.sql
@@ -1,2 +1,2 @@
 alter table etablissementinformations CHANGE R12320_ETABLISSEMENTINFORMATIONS R14320_ETABLISSEMENTINFORMATIONS tinyint;
-
+UPDATE prescriptionarticleliste set LIBELLE_ARTICLE = 'R 123-20' where prescriptionarticleliste.LIBELLE_ARTICLE like 'R%123-20';
