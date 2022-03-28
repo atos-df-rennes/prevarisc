f6817684371edd1c98c6e887cdca993fb7a70e59
valentinrouxelrennes <valentin.rouxel@atos.net>
2022-03-28 15:16:00 +0200

    rectif sql

--- sql/migrations/2.5-46-maj-article.sql
+++ sql/migrations/2.5-46-maj-article.sql
@@ -1,2 +1,3 @@
 alter table etablissementinformations CHANGE R12320_ETABLISSEMENTINFORMATIONS R14320_ETABLISSEMENTINFORMATIONS tinyint;
-UPDATE prescriptionarticleliste set LIBELLE_ARTICLE = 'R 123-20' where prescriptionarticleliste.LIBELLE_ARTICLE like 'R%123-20';
+UPDATE prescriptionarticleliste set LIBELLE_ARTICLE = 'R 143-20' where prescriptionarticleliste.LIBELLE_ARTICLE like 'R%123-20';
+UPDATE prescriptionarticleliste set LIBELLE_ARTICLE = 'R 143-16' where prescriptionarticleliste.LIBELLE_ARTICLE like 'R%123-16';
