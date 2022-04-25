f039662d12e54e07022da12b63a9595bec48d320
valentinrouxelrennes <valentin.rouxel@atos.net>
2022-04-25 17:24:28 +0200

    reconstruction generateur formulaire front

--- application/models/DbTable/Champ.php
+++ application/models/DbTable/Champ.php
@@ -5,15 +5,14 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
     protected $_name = 'champ'; // Nom de la base
     protected $_primary = 'ID_CHAMP'; // Clé primaire
 
-    public function getTypeChamp(int $idChamp): array{
+    public function getTypeChamp(int $idChamp){
         $select = $this->select()
         ->setIntegrityCheck(false)
         ->from(['c' => 'champ'], ['ID_CHAMP', 'NOM', 'ID_TYPECHAMP'])
         ->join(['ltcr' => 'listetypechamprubrique'], 'c.ID_TYPECHAMP = ltcr.ID_TYPECHAMP', ['TYPE'])
         ->where('c.ID_CHAMP = ?', $idChamp)
         ;
-
-    return $this->fetchAll($select)->toArray()[0];
+        return $this->fetchRow($select);
     }
 
     public function getChampsByRubrique(int $idRubrique): array
@@ -26,22 +25,6 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
             ->where('r.ID_RUBRIQUE = ?', $idRubrique)
             ->where('c.ID_PARENT IS NULL')
             ;
-
-        return $this->fetchAll($select)->toArray();
-    }
-
-    public function getChampsByRubriqueWithParent(int $idRubrique): array
-    {
-        $select = $this->select()
-            ->setIntegrityCheck(false)
-            ->from(['c' => 'champ'], ['ID_CHAMP','ID_PARENT', 'NOM', 'ID_TYPECHAMP'])
-            ->joinLeft(['v' => 'valeur'], 'c.ID_CHAMP = v.ID_CHAMP')
-            ->join(['r' => 'rubrique'], 'c.ID_RUBRIQUE = r.ID_RUBRIQUE', [])
-            ->join(['ltcr' => 'listetypechamprubrique'], 'c.ID_TYPECHAMP = ltcr.ID_TYPECHAMP', ['TYPE'])
-            ->where('r.ID_RUBRIQUE = ?', $idRubrique)
-            ->where('c.ID_PARENT IS NOT NULL')
-            ;
-        
         return $this->fetchAll($select)->toArray();
     }
 
@@ -63,22 +46,6 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
         return $this->fetchAll($select)->toArray();
     }
 
-    public function getChampAndJoinsWithParent(int $idChamp, bool $hasList = false): array
-    {
-        $select = $this->select()
-            ->setIntegrityCheck(false)
-            ->from(['c' => 'champ'], ['ID_CHAMP', 'NOM'])
-            ->join(['ltcr' => 'listetypechamprubrique'], 'c.ID_TYPECHAMP = ltcr.ID_TYPECHAMP', ['TYPE'])
-            ->join(['r' => 'rubrique'], 'c.ID_RUBRIQUE = r.ID_RUBRIQUE', ['ID_RUBRIQUE'])
-            ->where('c.ID_CHAMP = ?', $idChamp)
-        ;
-
-        if (true === $hasList) {
-            $select->joinLeft(['cvl' => 'champvaleurliste'], 'c.ID_CHAMP = cvl.ID_CHAMP', ['VALEUR']);
-        }
-        return $this->fetchAll($select)->toArray();
-    }
-
     public function getChampFromParent(int $idParent):array
     {
         $select = $this->select()
@@ -90,7 +57,6 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
 
         return $this->fetchAll($select)->toArray();
     }
-
 
     public function getValueChampList(int $idChamp):array{
         $select = $this->select()
@@ -107,16 +73,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
         ->from(['c' => 'champ'], ['ID_CHAMP', 'ID_PARENT', 'ID_TYPECHAMP'])
         ->join(['v' => 'valeur'], 'c.ID_CHAMP = v.ID_CHAMP',["VALEUR"=>"VALEUR_STR"])
         ->where('c.ID_CHAMP = ?', $idChamp);
-    return isset($this->fetchAll($select)->toArray()[0]['VALEUR']) ? $this->fetchAll($select)->toArray()[0]['VALEUR'] :'';
-    }
-
-    public function getSelectListOfChamp(int $idChamp):array{
-        $select = $this->select()
-            ->setIntegrityCheck(false)
-            ->from(['c' => 'champ'], ['ID_CHAMP', 'ID_PARENT', 'ID_TYPECHAMP'])
-            ->join(['cvl' => 'champvaleurliste'], 'c.ID_CHAMP = cvl.ID_CHAMP')
-            ->where('c.ID_CHAMP = ?', $idChamp);
-        return $this->fetchAll($select)->toArray();
+        return isset($this->fetchAll($select)->toArray()[0]['VALEUR']) ? $this->fetchAll($select)->toArray()[0]['VALEUR'] :'';
     }
 
     public function getChampFilsValue(int $idParent):array{
@@ -145,6 +102,53 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
             array_push($res, $tmpChamp);
         }
        
+        return $res;
+    }
+
+
+    public function getCorpFormulaire(int $idCapsuleRubrique){
+        $res = array();
+
+        $selectRubriqueForm = $this->select()
+            ->setIntegrityCheck(false)
+            ->from(['c' => 'champ'], ['ID_CHAMP','ID_PARENT', 'NOM', 'ID_TYPECHAMP'])
+            ->join(['r' => 'rubrique'],'r.ID_RUBRIQUE = c.ID_RUBRIQUE',['r.ID_RUBRIQUE'])
+            ->join(['ltcr' => 'listetypechamprubrique'],'ltcr.ID_TYPECHAMP = c.ID_TYPECHAMP')
+            ->where('c.ID_PARENT IS NULL')
+            ->where('r.ID_CAPSULERUBRIQUE = ?',$idCapsuleRubrique);
+
+        foreach ($this->fetchAll($selectRubriqueForm)->toArray() as $rubrique) {
+            if($rubrique['TYPE'] === 'Parent'){
+                $rubrique['CHAMP_FILS'] = $this->getChampFromParent($rubrique['ID_CHAMP']);
+                foreach ($rubrique['CHAMP_FILS'] as &$champFils) {
+                    if($champFils['TYPE'] === 'Liste'){
+                        $champFils['VALEUR'] = $this->getValueChampList($champFils['ID_CHAMP']);
+                    }
+                }    
+            }
+            if($rubrique['TYPE'] === 'Liste'){
+                $rubrique['VALEUR'] = $this->getValueChampList($rubrique['ID_CHAMP']);
+            }
+              
+            $res[$rubrique['ID_RUBRIQUE']][$rubrique['ID_CHAMP']] = $rubrique;
+        }
+        return $res;
+    }
+
+    public function getValeurFormulaire(int $idEtablissement, int $idCapsuleRubrique){
+        $select = $this->select()
+            ->setIntegrityCheck(false)
+            ->from(['c' => 'champ'], ['ID_CHAMP', 'NOM'])
+            ->join(['v' => 'valeur'],'v.ID_CHAMP = c.ID_CHAMP',["VALEUR_STR","VALEUR_LONG_STR","VALEUR_INT","VALEUR_CHECKBOX"])
+            ->join(['ev' => 'etablissementvaleur'],'ev.ID_VALEUR = v.ID_VALEUR')
+            ->join(['r' => 'rubrique'],'r.ID_RUBRIQUE = c.ID_RUBRIQUE')
+            ->where('ev.ID_ETABLISSEMENT = ?',$idEtablissement)
+            ->where('r.ID_CAPSULERUBRIQUE = ?',$idCapsuleRubrique);
+
+        $res = array();
+        foreach ($this->fetchAll($select)->toArray() as $valeur) {
+            $res[$valeur['ID_CHAMP']]  = $valeur;
+        }
         return $res;
     }
 }
