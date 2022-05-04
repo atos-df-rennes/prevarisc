23e84068b363e70b912b052a63a117c669ca285d
valentinrouxelrennes <valentin.rouxel@atos.net>
2022-05-04 11:43:03 +0200

    ajout generation  convoc

--- application/controllers/CalendrierDesCommissionsController.php
+++ application/controllers/CalendrierDesCommissionsController.php
@@ -1029,6 +1029,15 @@ class CalendrierDesCommissionsController extends Zend_Controller_Action
                     'descriptifVerificationsTechniques' => $rubriquesDossier,
                 ];
 
+                // Gestion des formulaires personnalisés
+                $rubriquesDossier = $this->serviceDescriptifDossier->getRubriques($listeDossiers[$val]['ID_DOSSIER'], 'Dossier');
+                $rubriquesEtablissement = $this->serviceDescriptifEtablissement->getRubriques($listeDossiers[$val]['infosEtab']['general']['ID_ETABLISSEMENT'], 'Etablissement');
+
+                $rubriquesByCapsuleRubrique = [
+                    'descriptifEtablissement' => $rubriquesEtablissement,
+                    'descriptifVerificationsTechniques' => $rubriquesDossier,
+                ];
+
                 // Gestion des rubriques/champs personnalisés
                 $capsulesRubriques = $this->serviceFormulaire->getAllCapsuleRubrique();
 
