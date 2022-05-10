155fba8206da970f95490ca0e3fe57ac882372b0
valentinrouxelrennes <valentin.rouxel@atos.net>
2022-05-10 16:19:55 +0200

    ajout dnd edit rubrique + preparation obj envoie requete

--- application/controllers/FormulaireController.php
+++ application/controllers/FormulaireController.php
@@ -54,7 +54,12 @@ class FormulaireController extends Zend_Controller_Action
     {
         $this->_helper->layout->setLayout('menu_admin');
         $this->view->inlineScript()->appendFile('/js/formulaire/rubrique.js', 'text/javascript');
+        $this->view->inlineScript()->appendFile('/js/formulaire/ordonnancement/ordonnancement.js', 'text/javascript');
+        $this->view->inlineScript()->appendFile('/js/formulaire/ordonnancement/Sortable.js', 'text/javascript');
+
         $this->view->headLink()->appendStylesheet('/css/formulaire/formulaire.css', 'all');
+        $this->view->headLink()->appendStylesheet('/css/formulaire/edit-table.css', 'all');
+
 
         $fieldForm = new Form_CustomFormField();
 
