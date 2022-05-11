e48960c876464d907b0b0db57a39b0574d351b91
valentinrouxelrennes <valentin.rouxel@atos.net>
2022-05-11 13:05:00 +0200

    IDX -> idfix

--- application/models/DbTable/Champ.php
+++ application/models/DbTable/Champ.php
@@ -26,7 +26,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
             ->join(['ltcr' => 'listetypechamprubrique'], 'c.ID_TYPECHAMP = ltcr.ID_TYPECHAMP', ['TYPE'])
             ->where('r.ID_RUBRIQUE = ?', $idRubrique)
             ->where('c.ID_PARENT IS NULL')
-            ->order('c.IDX asc')
+            ->order('c.idx asc')
             ;
 
         return $this->fetchAll($select)->toArray();
@@ -41,7 +41,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
             ->join(['r' => 'rubrique'], 'c.ID_RUBRIQUE = r.ID_RUBRIQUE', ['ID_RUBRIQUE'])
             ->where('c.ID_CHAMP = ?', $idChamp)
             ->where('c.ID_PARENT IS NULL')
-            ->order('c.IDX asc')
+            ->order('c.idx asc')
         ;
 
         if (true === $hasList) {
@@ -59,7 +59,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
             ->join(['c2' => 'champ'], 'c2.ID_PARENT = c.ID_CHAMP')
             ->join(['ltcr' => 'listetypechamprubrique'], 'ltcr.ID_TYPECHAMP = c2.ID_TYPECHAMP')
             ->where('c.ID_CHAMP = ?', $idParent)
-            ->order('c2.IDX asc')
+            ->order('c2.idx asc')
 
         ;
         return $this->fetchAll($select)->toArray();
@@ -100,7 +100,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
                 ->join(['r' => 'rubrique'], 'c.ID_RUBRIQUE = r.ID_RUBRIQUE', [])
                 ->join(['ltcr' => 'listetypechamprubrique'], 'c.ID_TYPECHAMP = ltcr.ID_TYPECHAMP', ['TYPE'])
                 ->where('c.ID_PARENT = ?', $idParent)
-                ->order('c.IDX asc')
+                ->order('c.idx asc')
             ;
 
         if (false !== strpos('Dossier', $aClass)) {
@@ -108,7 +108,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
                 ->join(['dv' => 'dossiervaleur'], 'dv.ID_VALEUR = v.ID_VALEUR')
                 ->join(['d' => 'dossier'], 'dv.ID_DOSSIER = d.ID_DOSSIER')
                 ->where('d.ID_DOSSIER = ?', $idEntity)
-                ->order('c.IDX asc')
+                ->order('c.idx asc')
                 ;
         }
         if (false !== strpos('Etablissement', $aClass)) {
@@ -154,7 +154,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
             ->join(['ltcr' => 'listetypechamprubrique'], 'ltcr.ID_TYPECHAMP = c.ID_TYPECHAMP')
             ->where('c.ID_PARENT IS NULL')
             ->where('r.ID_CAPSULERUBRIQUE = ?', $idCapsuleRubrique)
-            ->order('c.IDX asc')
+            ->order('c.idx asc')
         ;
 
         foreach ($this->fetchAll($selectRubriqueForm)->toArray() as $champ) {
@@ -189,7 +189,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
                     ->join(['r' => 'rubrique'], 'r.ID_RUBRIQUE = c.ID_RUBRIQUE')
                     ->where('ev.ID_ETABLISSEMENT = ?', $idEntity)
                     ->where('r.ID_CAPSULERUBRIQUE = ?', $idCapsuleRubrique)
-                    ->order('c.IDX asc')
+                    ->order('c.idx asc')
                 ;
 
                 break;
@@ -202,7 +202,7 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
                     ->join(['r' => 'rubrique'], 'r.ID_RUBRIQUE = c.ID_RUBRIQUE')
                     ->where('ev.ID_ETABLISSEMENT = ?', $idEntity)
                     ->where('r.ID_CAPSULERUBRIQUE = ?', $idCapsuleRubrique)
-                    ->order('c.IDX asc')
+                    ->order('c.idx asc')
                 ;
 
                 break;
@@ -216,11 +216,11 @@ class Model_DbTable_Champ extends Zend_Db_Table_Abstract
         return $res;
     }
 
-    //postParam => ['IDX' = nouvelle IDX champ, 'ID_CHAMP' => ID du champ]
+    //postParam => ['idx' = nouvelle idx champ, 'ID_CHAMP' => ID du champ]
     public function updateNewIdx($postParam):void
     {
         $champ = $this->find($postParam['ID_CHAMP'])->current();
-        $champ->IDX = $postParam['IDX'];
+        $champ->idx = $postParam['idx'];
         $champ->save();
         echo(json_encode($champ));
     }
