a4c98fb33763fdcdf7fb9d2e681eec6df176d154
Maxime Merrien <maxime.merrien@atos.net>
2022-08-01 17:03:32 +0200

    Ajout de règles et passage sur tout le projet

--- rector.php
+++ rector.php
@@ -4,26 +4,22 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Php71\Rector\FuncCall\CountOnNullRector;
-use Rector\Php74\Rector\Property\TypedPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     // get parameters
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
-        __DIR__ . '/application/models',
+        __DIR__ . '/application',
     ]);
 
     // Define what rule sets will be applied
     $containerConfigurator->import(LevelSetList::UP_TO_PHP_71);
+    $containerConfigurator->import(SetList::DEAD_CODE);
+
     $parameters->set(Option::SKIP, [
         CountOnNullRector::class
     ]);
-
-    // get services (needed for register a single rule)
-    // $services = $containerConfigurator->services();
-
-    // register a single rule
-    // $services->set(TypedPropertyRector::class);
 };
