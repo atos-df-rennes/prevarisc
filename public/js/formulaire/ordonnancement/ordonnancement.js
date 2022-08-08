2e7f55f6203f61e145ebd86c5ba1830ce8aa07aa
Maxime Merrien <maxime.merrien@atos.net>
2022-08-08 14:47:59 +0200

    Suppression de code mort & Simplification du js

--- public/js/formulaire/ordonnancement/ordonnancement.js
+++ public/js/formulaire/ordonnancement/ordonnancement.js
@@ -1,23 +1,19 @@
-window.onload = function(){
-
+window.onload = function() {
     const listGrp = document.getElementsByClassName('grp')
-    const listParent = document.getElementsByClassName('parent')
-    const listMain = document.getElementsByClassName('main')
-
     let originList = []
 
-    const getLisOrderInput = (idInputElem = null) => {
-        const listChampClass = ['inputChamp', 'parent','TR']
+    const getListOrderInput = (idInputElem = null) => {
         let toReturn = []
-        Array
-            .from(document.getElementById(idInputElem).children)
-                .filter(ch => listChampClass.includes(ch.tagName)).forEach(el =>{
-                    toReturn.push({
-                        //Nouvelle position
-                        idx : (Array.from(document.getElementById(idInputElem).children).filter(ch => listChampClass.includes(ch.tagName))).indexOf(el),
-                        ID : el.id
-                    })
-                })
+
+        const tableRows = document.querySelectorAll('#' + idInputElem + ' tr')
+        Array.from(tableRows).forEach(el => {
+            toReturn.push({
+                // Nouvelle position
+                idx: Array.from(tableRows).indexOf(el),
+                ID: el.id
+            })
+        })
+
         return toReturn
     }
     
@@ -45,48 +41,26 @@ window.onload = function(){
             })
     }
 
-    for(let i = 0; i< listGrp.length; i++){
+    for (let i = 0; i < listGrp.length; i++) {
         new Sortable.create(document.getElementById(listGrp[i].id), {
             group: listGrp[i].id,
             animation: 100
-          });
+        })
 
-          document.getElementById(listGrp[i].id).addEventListener(
-              'dragend',
-              function(){
-                    compareAndRequest(getLisOrderInput(listGrp[i].id))
-                },
-              false
-          )
-
-          document.getElementById(listGrp[i].id).addEventListener(
-            'dragstart',
-            function(){
-                originList = getLisOrderInput(listGrp[i].id)
-              },
+        document.getElementById(listGrp[i].id).addEventListener(
+            'dragend',
+            function() {
+                compareAndRequest(getListOrderInput(listGrp[i].id))
+            },
             false
         )
-    }
 
-    for(let i = 0; i< listParent.length; i++){
-        new Sortable.create(document.getElementById(listParent[i].id), {
-            group:listParent[i].id,
-            animation: 100
-          });
-          document.getElementById(listParent[i].id).addEventListener(
-            'dragend',
-            function(){
-                getLisOrderInput(listParent[i].id)
+        document.getElementById(listGrp[i].id).addEventListener(
+            'dragstart',
+            function() {
+                originList = getListOrderInput(listGrp[i].id)
             },
             false
         )
     }
-      
-    for(let i = 0; i< listMain.length; i++){
-        new Sortable.create(document.getElementById(listMain[i].id), {
-            group:listMain[i].id,
-            animation: 100
-          });
-    }
-
 }
