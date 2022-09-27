1aff78a895c3a9974ff9cfff1a6fd51ebe054f72
Valentin Rouxel <valentin.rouxel@atos.net>
2022-09-27 11:13:45 +0200

    insert new data ok

--- public/js/formulaire/tableau/gestionTableau.js
+++ public/js/formulaire/tableau/gestionTableau.js
@@ -1,21 +1,13 @@
 $(document).ready(function() {  
     
     $('.addRow').click(function() {
-        //Envoie un ajout de ligne en db a supprimer une fois fini
-        /*
-        $.ajax({
-            type: "POST",
-            url: "/formulaire/add-row-table",
-            data: {ID_CHAMP:$(this).attr('idparent'),ID_ENTITY:document.location.href.split('/')[6],ENTITY:document.location.href.split('/')[3].charAt(0).toUpperCase()+document.location.href.split('/')[3].slice(1) ,idx:document.getElementById('tbody-'+$(this).attr('idparent')).children.length},
-            dataType: "json"
-        }).then(() => {*/
-            
+           
         //Ajout de la nouvelle ligne au tableau selectionne
         document.getElementById('tbody-'+$(this).attr('idparent')).innerHTML 
         +=
-        ( 
-        document.getElementById('tbody-'+$(this).attr('idparent')).children[0].innerHTML
-        )
+            ( 
+                document.getElementById('tbody-'+$(this).attr('idparent')).children[0].innerHTML
+            )
         
         //recuperation du tr comprennant tous les inputs
         let inputInTr = 
@@ -28,7 +20,9 @@ $(document).ready(function() {
         const newD = Date.now()
         Array.from(inputInTr).forEach(input => {
             if(input.name){
-                //input.setAttribute("name",'valeur-'+newD+'-'+input.name.split('-')[input.name.split('-').length - 3]+'-'+input.name.split('-')[input.name.split('-').length - 2]+'-'+input.name.split('-')[input.name.split('-').length - 1])
+                let strSplit = input.name.split('-')
+                newStr = strSplit[0] + '-' + newD + '-' + strSplit[2] + '-' + strSplit[3] + '-NULL'
+                input.setAttribute("name",newStr)
             }
         })
 
@@ -36,7 +30,6 @@ $(document).ready(function() {
             $(this).closest('tr').remove();
         })
 
-        //})
     })
 
     $('.deleteRow').click(function(){
