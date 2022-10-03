bb2f8b813f6c8f9d27d0eebb83e08a816e83adce
Maxime Merrien <maxime.merrien@atos.net>
2022-10-03 12:00:07 +0200

    Correction de l'affectation de l'id de la valeur

--- public/js/formulaire/tableau/gestionTableau.js
+++ public/js/formulaire/tableau/gestionTableau.js
@@ -9,21 +9,30 @@ $(document).ready(function() {
                 document.getElementById('tbody-'+$(this).attr('idparent')).children[0].innerHTML
             )
         
-        //recuperation du tr comprennant tous les inputs
-        let inputInTr = 
+        const parentTableBody = document.getElementById('tbody-'+$(this).attr('idparent'))
+        const parentTableNewLine = parentTableBody.getElementsByTagName('tr')[
             document.getElementById('tbody-'+$(this).attr('idparent'))
-                        .getElementsByTagName('tr')
-                            [document.getElementById('tbody-'+$(this).attr('idparent'))
-                                        .getElementsByTagName('tr').length -1 ]
-                                        .getElementsByTagName('input')
+                    .getElementsByTagName('tr').length -1
+        ]
+
+        //recuperation du tr comprennant tous les inputs
+        const inputsInNewLine = parentTableNewLine.getElementsByTagName('input')
+        const textareasInNewLine = parentTableNewLine.getElementsByTagName('textarea')
+        const selectsInNewLine = parentTableNewLine.getElementsByTagName('select')
+
+        const formElements = [inputsInNewLine, textareasInNewLine, selectsInNewLine]
+
         //Creation du timestamp pour grouper la ligne des inputs
         const newD = Date.now()
-        Array.from(inputInTr).forEach(input => {
-            if(input.name){
-                let strSplit = input.name.split('-')
-                newStr = strSplit[0] + '-' + newD + '-' + strSplit[2] + '-' + strSplit[3] + '-NULL'
-                input.setAttribute("name",newStr)
-            }
+
+        formElements.forEach(formElement => {
+            Array.from(formElement).forEach(input => {
+                if(input.name){
+                    let strSplit = input.name.split('-')
+                    newStr = strSplit[0] + '-' + newD + '-' + strSplit[2] + '-' + strSplit[3] + '-NULL'
+                    input.setAttribute("name",newStr)
+                }
+            })
         })
 
         $('.deleteRow').click(function(){
