3b45bee0040c024227152e6f69d499870b8736a0
Maxime Merrien <maxime.merrien@atos.net>
2022-10-04 17:03:22 +0200

    Les rubriques ne s'affichaient pas selon les choix de l'utilisateur

--- application/models/DbTable/DisplayRubriqueEtablissement.php
+++ application/models/DbTable/DisplayRubriqueEtablissement.php
@@ -5,14 +5,17 @@ class Model_DbTable_DisplayRubriqueEtablissement extends Zend_Db_Table_Abstract
     protected $_name = 'displayrubriqueetablissement'; // Nom de la base
     protected $_primary = ['ID_ETABLISSEMENT', 'ID_RUBRIQUE']; // Clé primaire
 
-    public function getUserDisplay(int $idEtablissement, int $idRubrique): array
+    /**
+     * @return null|Zend_Db_Table_Row_Abstract
+     */
+    public function getUserDisplay(int $idEtablissement, int $idRubrique)
     {
         $select = $this->select()
-            ->from(['dre' => 'displayrubriqueetablissement'], ['ID_RUBRIQUE', 'USER_DISPLAY'])
+            ->from(['dre' => 'displayrubriqueetablissement'], ['USER_DISPLAY'])
             ->where('dre.ID_ETABLISSEMENT = ?', $idEtablissement)
             ->where('dre.ID_RUBRIQUE = ?', $idRubrique)
         ;
 
-        return $this->getAdapter()->fetchAll($select);
+        return $this->getAdapter()->fetchRow($select);
     }
 }
