955fc7f38a09131b7eb96b01c694b8f2aade5eec
Maxime Merrien <maxime.merrien@atos.net>
2023-05-19 10:47:30 +0200

    Plugin libellé Observations dossier

---
+++ application/plugins/LibelleDossierObservations.php
@@ -0,0 +1,23 @@
+<?php
+
+class Plugin_LibelleDossierObservations extends Zend_Controller_Plugin_Abstract
+{
+    public function postDispatch(Zend_Controller_Request_Abstract $request)
+    {
+        if ($request->getControllerName() !== 'dossier' || $request->getActionName() !== 'index') {
+            return;
+        }
+
+        $view = Zend_Controller_Front::getInstance()
+            ->getParam('bootstrap')
+            ->getResource('view');
+
+        $view->inlineScript()->appendScript(
+<<<EOS
+$(document).ready(function() {
+    $('#OBSERVATION .libelle').text('Observations introduction')
+})
+EOS
+        );
+    }
+}
