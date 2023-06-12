c922c84f435dc34420b11a3976e3fb5af124f9a3
Isidore Prevaux <isidore.prevaux.external@atos.net>
2023-06-12 16:04:24 +0200

    separation en deux actions

--- application/controllers/ListeDesEvolutionsController.php
+++ application/controllers/ListeDesEvolutionsController.php
@@ -9,16 +9,24 @@ class ListeDesEvolutionsController extends Zend_Controller_Action
             throw new Exception('Layout does not have the correct type.');
         }
         $layout->getLayoutInstance()->setLayout('menu_admin');
-
         $json = file_get_contents(CONFIG_PATH.DS.'liste-evols.json');
         $parsjson = json_decode($json);
         $datas = (array) $parsjson;
-        if (!empty($_GET)) {
-            foreach (array_keys($datas) as $key) {
-                $datas[$key][0] = isset($_GET[$key]);
-            }
-        }
-        file_put_contents(CONFIG_PATH.DS.'liste-evols.json', json_encode($datas));
         $this->view->assign('datas', $datas);
+    }
+
+    public function editAction()
+    {
+        $this->_helper->viewRenderer->setNoRender();
+        $json = file_get_contents(CONFIG_PATH.DS.'liste-evols.json');
+        $parsjson = json_decode($json);
+        $datas = (array) $parsjson;
+        if (!empty($_POST)) {
+            foreach (array_keys($datas) as $key) {
+                $datas[$key][0] = isset($_POST[$key]);
+            }
+            file_put_contents(CONFIG_PATH.DS.'liste-evols.json', json_encode($datas));
+        }
+        $this->_helper->redirector('index', null, null, []);
     }
 }
