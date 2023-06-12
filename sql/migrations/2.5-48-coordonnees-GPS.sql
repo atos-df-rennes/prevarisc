d25793181fcd9ff00022ab23078c51aa241b35b3
Maxime Merrien <maxime.merrien@atos.net>
2023-06-12 16:25:24 +0200

    On fixe la précision à 6 décimales en front

--- sql/migrations/2.5-48-coordonnees-GPS.sql
+++ sql/migrations/2.5-48-coordonnees-GPS.sql
@@ -1,4 +1,4 @@
 SET NAMES 'utf8';
 
-ALTER TABLE etablissementadresse  MODIFY LON_ETABLISSEMENTADRESSE float(9,6); 
-ALTER TABLE etablissementadresse  MODIFY LAT_ETABLISSEMENTADRESSE float(9,7);
+ALTER TABLE `etablissementadresse` MODIFY `LON_ETABLISSEMENTADRESSE` float(9,6); 
+ALTER TABLE `etablissementadresse` MODIFY `LAT_ETABLISSEMENTADRESSE` float(8,6);
