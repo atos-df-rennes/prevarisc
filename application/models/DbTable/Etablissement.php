3f45d098f1ed1197e352fff1612d6fca8d7e1bd3
Maxime Merrien <maxime.merrien@atos.net>
2023-06-19 10:58:14 +0200

    Le dossier levé n'influe pas sur l'affichage dans l'onglet de l'établissement

--- application/models/DbTable/Etablissement.php
+++ application/models/DbTable/Etablissement.php
@@ -470,7 +470,7 @@ class Model_DbTable_Etablissement extends Zend_Db_Table_Abstract
             ->join(['ed' => 'etablissementdossier'], 'd.ID_DOSSIER = ed.ID_DOSSIER', [])
             ->join(['e' => 'etablissement'], 'ed.ID_ETABLISSEMENT = e.ID_ETABLISSEMENT', [])
             ->join(['ad' => 'avisderogations'], 'd.ID_DOSSIER = ad.ID_DOSSIER')
-            ->join(['d2' => 'dossier'], 'd2.ID_DOSSIER = ad.ID_DOSSIER_LIE', ['TYPE_DOSSIER', 'DATECOMM_DOSSIER', 'DATEVISITE_DOSSIER'])
+            ->joinLeft(['d2' => 'dossier'], 'd2.ID_DOSSIER = ad.ID_DOSSIER_LIE', ['TYPE_DOSSIER', 'DATECOMM_DOSSIER', 'DATEVISITE_DOSSIER'])
             ->where('e.ID_ETABLISSEMENT = ?', $idEtablissement)
             ->where('ad.DISPLAY_HISTORIQUE = ?', 1)
         ;
