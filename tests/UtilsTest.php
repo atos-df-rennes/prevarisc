6ff14452e8b7b485f7bc8e89731e1af00c9a0cd1
Maxime Merrien <maxime.merrien@atos.net>
2023-08-03 18:39:40 +0200

    Test avec le magical case

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -5,14 +5,27 @@ use PHPUnit\Framework\TestCase;
 
 final class UtilsTest extends TestCase
 {
+    private $utils;
+
+    public function setUp()
+    {
+        $this->utils = new Utils();
+    }
+
     /**
      * @dataProvider fusionNameProvider
      */
     public function testGetDescriptifFieldFusionName(string $baseObject, array $options, string $expected): void
     {
-        $utilsService = new Utils();
+        $this->assertSame($expected, $this->utils->getFullFusionName($baseObject, $options));
+    }
 
-        $this->assertSame($expected, $utilsService->getFullFusionName($baseObject, $options));
+    /**
+     * @dataProvider magicalNameProvider
+     */
+    public function testGetDescriptifFieldMagicalName(string $initialName, string $expected): void
+    {
+        $this->assertSame($expected, $this->utils->getFusionNameMagicalCase($initialName));
     }
 
     public function fusionNameProvider(): array
@@ -20,6 +33,14 @@ final class UtilsTest extends TestCase
         return [
             'simple field with rubrique' => ['descriptifEtablissement', ['Ma Rubrique', 'Mon Champ'], 'descriptifEtablissement-ma_rubrique-mon_champ'],
             'formatted field without rubrique' => ['descriptifEtablissement', ['Mon Champ d\'enfer "Personnalisé"'], 'descriptifEtablissement-mon_champ_d_enfer__personnalisé_'],
+        ];
+    }
+
+    public function magicalNameProvider(): array
+    {
+        return [
+            'simple field' => ['descriptif Rubrique Un champ', 'DescriptifRubriqueUnChamp'],
+            'formatted field' => ['descriptif rUbrique un Champ d\'enfer "Personnalisé"', 'DescriptifRubriqueUnChampDEnferPersonnalisé'],
         ];
     }
 }
