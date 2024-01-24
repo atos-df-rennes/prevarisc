4eb8e8c3c5d7e997875696bcb4bdf7fcc87ac73f
Maxime Merrien <maxime.merrien@atos.net>
2024-01-24 14:15:07 +0100

    [#4886] Correction du type de dossier "levée de réserves suite à un avis défavorable"

--- sql/migrations/2.5-40-export_calc_dossiers.sql
+++ sql/migrations/2.5-40-export_calc_dossiers.sql
@@ -1,4 +1,3 @@
 SET NAMES 'utf8';
 
 INSERT INTO `privileges`(`id_privilege`,`name`, `text`,`id_resource`) VALUES(210,"export_doss", "Dossiers",200);
-UPDATE `dossiernatureliste` SET  `ID_DOSSIERTYPE` =  2 WHERE  `dossiernatureliste`.`ID_DOSSIERNATURE` = 19;
---
+++ sql/migrations/2.5-40-fix-update-dossiernatureliste.sql
@@ -0,0 +1,3 @@
+SET NAMES 'utf8';
+
+UPDATE `dossiernatureliste` SET `ID_DOSSIERTYPE` = 1 WHERE `dossiernatureliste`.`ID_DOSSIERNATURE` = 19;
