a633ce92c2eea46cd1cb5acaeeadccefe032f0d5
Maxime Merrien <maxime.merrien@atos.net>
2025-09-11 10:32:42 +0200

    Ordonnancement des valeurs

--- application/models/DbTable/ChampValeurListe.php
+++ application/models/DbTable/ChampValeurListe.php
@@ -13,16 +13,12 @@ class Model_DbTable_ChampValeurListe extends Zend_Db_Table_Abstract
         $select = $this->select()
             ->setIntegrityCheck(false)
             ->from('champvaleurliste')
+            ->order('idx ASC')
+            ->order('ID_VALEURLISTE ASC')
         ;
 
         return $this->fetchAll($select)->toArray();
     }
-
-    // Il faut ajouter une colonne `idx` dans la table `champvaleurliste`
-    // Cette colonne permettra de stocker l'ordre des valeurs d'un champ.
-    // Grâce à cela, getValeurListeByChamp() pourra récupérer les valeurs
-    // triées selon l'ordre défini dans `idx`, ce qui est indispensable
-    // pour que l'affichage reflète correctement le drag & drop côté utilisateur.
 
     public function getValeurListeByChamp(int $idChamp): array
     {
@@ -31,6 +27,7 @@ class Model_DbTable_ChampValeurListe extends Zend_Db_Table_Abstract
             ->from(['cvl' => 'champvaleurliste'], ['ID_VALEURLISTE', 'VALEUR'])
             ->join(['c' => 'champ'], 'cvl.ID_CHAMP = c.ID_CHAMP', [])
             ->where('c.ID_CHAMP = ?', $idChamp)
+            ->order('cvl.idx ASC')
         ;
 
         return $this->fetchAll($select)->toArray();
