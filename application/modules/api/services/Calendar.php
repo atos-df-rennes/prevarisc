8ae2bec0ff2d2ac60de272c4afecffc3eeba134f
Maxime Merrien <maxime.merrien@atos.net>
2026-01-16 11:11:16 +0100

    fix: la méthode de récupération du maire attend forcément la commune de référence

--- application/modules/api/services/Calendar.php
+++ application/modules/api/services/Calendar.php
@@ -272,7 +272,11 @@ class Api_Service_Calendar
         $corpus .= self::LF.self::LF;
 
         $adresseService = new Service_Adresse();
-        $maire = $adresseService->getMaire($commissionEvent['NUMINSEE_COMMUNE']);
+        $maire = null;
+
+        if (null !== $commissionEvent['NUMINSEE_COMMUNE']) {
+            $maire = $adresseService->getMaire($commissionEvent['NUMINSEE_COMMUNE']);
+        }
         if ($maire && [] !== $maire) {
             $corpus .= sprintf(
                 'Coordonnées de la mairie :%s%s%s',
