eb0703c2227208326fde361d85a0bc6622104130
Maxime Merrien <maxime.merrien@atos.net>
2026-03-30 09:09:50 +0200

    fix: ne plante pas si pas de dernier dossier donnant avis

--- application/controllers/DossierController.php
+++ application/controllers/DossierController.php
@@ -1411,7 +1411,10 @@ class DossierController extends Zend_Controller_Action
                 $dbDossier = new Model_DbTable_Dossier();
                 // on récupère les infos du dernier dossier donnant avis de l'établissement courant
                 foreach ($listeEtab as $etab) {
-                    $dernierDossierDonnantAvis = $dbDossier->getGeneral($dbDossier->getDernierIdDossierDonnantAvis($etab['ID_ETABLISSEMENT'])['ID_DOSSIER']);
+                    $dernierIdDossierDonnantAvis = $dbDossier->getDernierIdDossierDonnantAvis($etab['ID_ETABLISSEMENT']);
+                    $dernierDossierDonnantAvis = false !== $dernierIdDossierDonnantAvis
+                        ? $dbDossier->getGeneral($dernierIdDossierDonnantAvis['ID_DOSSIER'])
+                        : false;
                     $service_dossier->saveDossierDonnantAvisCurrentEtab($dernierDossierDonnantAvis, $etab, $cache);
                 }
             }
@@ -3252,7 +3255,10 @@ class DossierController extends Zend_Controller_Action
             $dbDossier = new Model_DbTable_Dossier();
             // on récupère les infos du dernier dossier donnant avis de l'établissement courant
             foreach ($listeEtab as $etab) {
-                $dernierDossierDonnantAvis = $dbDossier->getGeneral($dbDossier->getDernierIdDossierDonnantAvis($etab['ID_ETABLISSEMENT'])['ID_DOSSIER']);
+                $dernierIdDossierDonnantAvis = $dbDossier->getDernierIdDossierDonnantAvis($etab['ID_ETABLISSEMENT']);
+                $dernierDossierDonnantAvis = false !== $dernierIdDossierDonnantAvis
+                    ? $dbDossier->getGeneral($dernierIdDossierDonnantAvis['ID_DOSSIER'])
+                    : false;
                 $service_dossier->saveDossierDonnantAvisCurrentEtab($dernierDossierDonnantAvis, $etab, $cache);
             }
 
