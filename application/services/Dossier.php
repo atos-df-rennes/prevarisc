eadbb1ab2e62b93c2512f1d36dbaeed705a1e04e
Maxime Merrien <maxime.merrien@atos.net>
2026-03-30 09:18:13 +0200

    chore: cs

--- application/services/Dossier.php
+++ application/services/Dossier.php
@@ -837,7 +837,9 @@ class Service_Dossier
     // Retrouve l'avis du dernier dossier donnant avis pour l'établissement courant uniquement
 
     /**
-     * @param false|array $dernierDossierDonnantAvis
+     * @param array|false $dernierDossierDonnantAvis
+     * @param mixed       $etab
+     * @param mixed       $cache
      */
     public function saveDossierDonnantAvisCurrentEtab($dernierDossierDonnantAvis, $etab, $cache): Zend_Db_Table_Row_Abstract
     {
