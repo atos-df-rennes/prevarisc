5e3dce3b2518ac1db584c9c2fb3c3cd9129f203d
Maxime MERRIEN <48253852+devottered@users.noreply.github.com>
2026-04-08 18:07:44 +0200

    migration(formulaires): édition des valeurs (#323)

--- application/controllers/EtablissementController.php
+++ application/controllers/EtablissementController.php
@@ -326,7 +326,7 @@ class EtablissementController extends Zend_Controller_Action
                 $this->_helper->flashMessenger(['context' => 'error', 'title' => 'Mise à jour annulée', 'message' => 'Les descriptifs n\'ont pas été mis à jour. Veuillez rééssayez. ('.$e->getMessage().')']);
             }
 
-            $this->redirect('/etablissement/'.$idEtablissement.'/descriptif');
+            $this->_helper->redirector('descriptif', null, null, ['id' => $idEtablissement]);
         }
     }
 
@@ -661,7 +661,7 @@ class EtablissementController extends Zend_Controller_Action
                 $this->_helper->flashMessenger(['context' => 'error', 'title' => 'Mise à jour annulée', 'message' => 'Les effectifs et dégagements n\'ont pas été mis à jour. Veuillez rééssayez. ('.$e->getMessage().')']);
             }
 
-            $this->redirect('/etablissement/'.$idEtablissement.'/effectifs-degagements');
+            $this->_helper->redirector('effectifs-degagements-etablissement', null, null, ['id' => $idEtablissement]);
         }
     }
 
